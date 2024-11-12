b78f32d757f08a1ea3de9868807424543df4aae9
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-11 23:46:14 -0600

    Imported google fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { Playfair_Display, Fredoka } from "next/font/google";
+
+const play = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-play",
+});
+
+const fredoka = Fredoka({
+  subsets: ["latin"],
+  variable: "--font-fredoka",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${play.variable} ${fredoka.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
