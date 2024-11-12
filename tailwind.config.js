8cfcd54d59141455018ca3b89d861278de99955e
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-11 23:45:26 -0600

    added required daisyui plugin , a custom theme, and darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,32 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#c08552",
+
+          secondary: "#8c523b",
+
+          accent: "#f2d6ce",
+
+          neutral: "#f2e7dd",
+
+          "base-100": "#fff9f2",
+
+          info: "#bae0da",
+
+          success: "#7a958f",
+
+          warning: "#ffc98b",
+
+          error: "#ca8e82",
+        },
+      },
+      "night",
+    ],
+  },
+  darkMode: ["selector", '[data-theme="night"]'],
+  plugins: [require("daisyui")],
 };
