781789a0e2d7c803aec55564d221ccc1b43e9c6c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-13 23:44:28 -0600

    added an about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h2>About Us</h2>
+      <Footer />
+    </div>
+  );
+}
