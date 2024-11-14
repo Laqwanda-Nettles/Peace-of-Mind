ecf08026ceadb5468b29fa990f55f7047722c0fa
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-13 23:30:33 -0600

    added tailwind css to component

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,10 +1,14 @@
 export default function QuoteCard({ author, quote }) {
   return (
-    <div>
+    <div className="bg-accent dark:bg-neutral mx-3 my-auto p-4 rounded-lg">
       <blockquote>
-        <p>{quote}</p>
+        <p className="before:content-['❝'] after:content-['❞'] before:mr-1 after:ml-1 text-xl">
+          {quote}
+        </p>
       </blockquote>
-      <p>{author}</p>
+      <p className="text-right before:content-['—'] before:mr-2 text-lg">
+        {author}
+      </p>
     </div>
   );
 }
