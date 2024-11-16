0897addb86763ce8f01983a436e5a785f2624dad
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-16 07:38:09 -0600

    added API route to get moods

---
+++ src/pages/api/get-moods.js
@@ -0,0 +1,30 @@
+import { Redis } from "@upstash/redis";
+
+const redis = Redis.fromEnv();
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      //Get the list of mood entries
+      const moodEntries = await redis.lrange("mood_entries", 0, -1);
+
+      //Parse entries
+      const parsedEntries = moodEntries.map((entry) => {
+        try {
+          return typeof entry === "string" ? JSON.parse(entry) : entry;
+        } catch (error) {
+          console.error("Error parsing entry: ", entry);
+          return entry; // Return the raw entry if parsing fails
+        }
+      });
+
+      res.status(200).json(parsedEntries);
+    } catch (error) {
+      console.error("Error retrieving mood data from Redis: ", error);
+      res.status(500).json({ error: "Failed to retrieve mood data." });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
