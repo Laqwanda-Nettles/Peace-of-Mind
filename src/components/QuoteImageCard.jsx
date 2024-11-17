5cf74cee3f1b6a1dd56fc69ff725def9cda81ed8
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-17 10:04:17 -0600

    removed zen quotes api and styled component with tailwind, daisyui, and custom css

--- src/components/QuoteImageCard.jsx
+++ src/components/QuoteImageCard.jsx
@@ -1,8 +1,15 @@
-const ZEN_URL = process.env.ZENQUOTES_URL;
-export default function QuoteImageCard() {
+export default function QuoteImageCard({ imgSrc, title, quote, author }) {
   return (
-    <div className="w-96">
-      <img src={ZEN_URL} alt="Inspirational Quote" />
+    <div className="card w-80 h-96 bg-base-100 shadow-xl relative rounded-2xl">
+      <figure className="w-full h-full rounded-2xl">
+        <img src={imgSrc} alt={title} className="object-cover w-full h-full" />
+        {/* Background overlay for opacity */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 rounded-2xl"></div>
+      </figure>
+      <div className="card-body absolute w-full text-white">
+        <h2 className="card-title text-2xl text-shadow">{quote}</h2>
+        <p className="italic text-right text-lg font-semibold">- {author}</p>
+      </div>
     </div>
   );
 }
