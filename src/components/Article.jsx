e527092a3a71cf7405ba8967240bcc10f5a5e8d0
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-17 10:01:33 -0600

    change link to anchor tag for external sites

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import PropTypes from "prop-types";
 
 export default function Article({
@@ -11,19 +10,19 @@ export default function Article({
 }) {
   return (
     <div className="card bg-white dark:bg-neutral dark:text-slate-50 w-96 shadow-xl">
-      <figure>
-        <img src={imgSrc} alt={title} />
-      </figure>
-      <div className="card-body">
-        <Link href={"url"}>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <figure>
+          <img src={imgSrc} alt={title} />
+        </figure>
+        <div className="card-body">
           <h2 className="card-title text-2xl font-semibold">{title}</h2>
-        </Link>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">{tag}</div>
-          <div className="badge badge-outline">{secondTag}</div>
+          <p>{description}</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">{tag}</div>
+            <div className="badge badge-outline">{secondTag}</div>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
