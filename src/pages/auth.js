49671cf8b03841f4467d1673d3c4a12f9c22a464
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-22 01:00:00 -0600

    created auth page to handle magic link authentication

---
+++ src/pages/auth.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function AuthPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    console.log("Search Params: ", searchParams.get("token", "email"));
+
+    const token = searchParams.get("token");
+    const email = searchParams.get("email");
+
+    const fetchAuth = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth?token=${token}&email=${email}`
+      );
+      const data = await response.json();
+      console.log(data);
+      if (response.status === 200) {
+        localStorage.setItem("token", token);
+        localStorage.setItem("email", email);
+        console.log("Auth Page status: ", response.status);
+        router.push("/journal");
+      } else {
+        router.push("/login");
+      }
+    };
+
+    fetchAuth();
+  }, [router]);
+
+  return (
+    <div className="mt-12 flex justify-center">
+      <h2 className="text-5xl font-extrabold text-secondary">
+        Authenticating <span className="loading loading-dots loading-lg"></span>
+      </h2>
+    </div>
+  );
+}
