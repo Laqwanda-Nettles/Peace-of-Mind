0a870889a718656648a03281338d09e8afaabd08
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-22 01:09:20 -0600

    adjusted API route to save mood and data data to Redis for authenticated users.

--- src/pages/api/save-mood.js
+++ src/pages/api/save-mood.js
@@ -4,15 +4,21 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { mood, date } = req.body;
+    const { mood, date, token } = req.body;
 
     if (!mood || !date) {
       return res.status(400).json({ error: "Mood and date are required." });
     }
 
+    const result = await redis.get(`magiclink:${token}`);
+    const email = result.email;
+
     try {
       // Push mood data to a Redis list
-      await redis.lpush("mood_entries", JSON.stringify({ mood, date }));
+      await redis.lpush(
+        `mood_entries:${email}`,
+        JSON.stringify({ mood, date })
+      );
       res.status(200).json({ message: "Mood saved successfully!" });
     } catch (error) {
       console.error("Error saving mood to Upstash: ", error);
