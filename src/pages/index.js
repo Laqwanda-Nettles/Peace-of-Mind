6f1346d8f197df1c37ba901214d03873e408e640
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-02-08 15:14:08 -0600

    Added internal API to replace the Ninja API

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,18 +6,24 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  const URL = "/api/quote";
+  const URL = "/api/quotes";
 
   const fetchQuotes = async () => {
     try {
       const result = await fetch(URL);
       const data = await result.json();
-      setQuotes(data);
+      setQuote(data);
     } catch (error) {
       console.error("Error fetching data from API: ", error);
+      setError(true);
+      setErrorMsg(
+        `Oops, looks like we hit a little snag! But remember, even the greatest journeys start with a single step, so lets try again.`
+      );
     } finally {
       setLoading(false);
     }
@@ -62,20 +68,16 @@ export default function Home() {
             You&apos;re not alone on your journey. Let Peace of Mind be your
             companion toward a balanced and fulfilled life.
           </p>
-          {loading ? (
+          {error ? (
+            <p>{errorMsg}</p>
+          ) : loading ? (
             <div className="flex gap-2">
               <p className="text-xl font-semibold">Loading Quote</p>
               <span className="loading loading-dots loading-lg"></span>
             </div>
           ) : (
             <div>
-              {quotes.map((quote, index) => (
-                <QuoteCard
-                  key={index}
-                  quote={quote.quote}
-                  author={quote.author}
-                />
-              ))}
+              {quote && <QuoteCard quote={quote.quote} author={quote.author} />}
             </div>
           )}
         </section>
